a0031a33eec14924672363b280ff6d3404ce96f4
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2023-01-19 16:16:04 +0530

    added cards

--- src/pages/contests/index.tsx
+++ src/pages/contests/index.tsx
@@ -8,7 +8,7 @@ const Contests = () => {
   return (
     <Layout>
       <Seo title='Contests' />
-      <div className='grid h-auto min-h-full w-full grid-cols-3  place-content-start place-items-center gap-x-[20px] gap-y-10 overflow-y-scroll rounded-[50px] bg-transparent p-8 pt-8 pb-5 text-black scrollbar-hide lg1300:pt-14 xl1400:pt-12 xl1500:pt-10'>
+      <div className='grid h-auto min-h-full w-full grid-cols-3  place-content-start place-items-center gap-x-[20px] gap-y-10 overflow-y-scroll rounded-[50px] bg-transparent p-8 pt-8 pb-5 text-black scrollbar-hide  xl1500:pt-10'>
         <ContestCards />
         <ContestCards />
         <ContestCards />
