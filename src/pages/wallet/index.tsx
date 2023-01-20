4732298ecc3daec444c111cc722f385539bff97f
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2023-01-20 22:14:27 +0530

    updated wallet page

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -10,34 +10,36 @@ const Wallet = () => {
   return (
     <Layout>
       <Seo title='Wallet' />
-      <div className='flex h-full w-full flex-col items-center justify-center'>
-        <div className='flex h-[90%] max-h-[500px] w-[55%] flex-col items-center justify-between'>
-          <BalanceContainer className='h-[55%] w-[70%]' />
-          <input
-            type='number'
-            placeholder='Enter topup/withdraw amount'
-            className={`${styles.input} box-border h-[55px] w-[85%] min-w-[200px] max-w-[320px] pl-5 text-black`}
-          />
-          <div className='mt-2 flex h-[20%] w-1/2 items-center justify-evenly'>
-            <div className='flex h-auto w-auto flex-col items-center justify-start'>
-              <button
-                className={`${styles.button} flex h-[80px] w-[80px] items-center justify-center rounded-[25px]`}
-              >
-                <ArrowUp />
-              </button>
-              <span className='mt-1 cursor-pointer font-poppins text-[1.1rem] font-[400] text-white'>
-                TopUp
-              </span>
-            </div>
-            <div className='ml-3 flex h-auto w-auto flex-col items-center justify-start'>
-              <button
-                className={`${styles.button} flex h-[80px] w-[80px] items-center justify-center rounded-[25px]`}
-              >
-                <ArrowDown />
-              </button>
-              <span className='mt-1 cursor-pointer font-poppins text-[1.1rem] font-[400] text-white'>
-                Withdraw
-              </span>
+      <div className='flex h-full w-full flex-col items-center justify-center p-10 xl1400:p-14 xxl3100:p-[130px]'>
+        <div className='flex h-full w-full flex-col items-center justify-center rounded-[40px] bg-bgPredictions shadow-predictions'>
+          <div className='flex h-[85%] max-h-[500px] w-[55%] flex-col items-center justify-between'>
+            <BalanceContainer className='h-[50%] w-[65%]' />
+            <input
+              type='number'
+              placeholder='Enter topup/withdraw amount'
+              className={`${styles.input} -mt-5 box-border h-[55px] w-[85%] min-w-[200px] max-w-[320px] pl-5 text-black`}
+            />
+            <div className='mt-2 flex h-[20%] w-1/2 items-center justify-evenly'>
+              <div className='flex h-auto w-auto flex-col items-center justify-start'>
+                <button
+                  className={`${styles.button} flex h-[80px] w-[80px] items-center justify-center rounded-[25px]`}
+                >
+                  <ArrowUp />
+                </button>
+                <span className='mt-1 cursor-pointer font-poppins text-[1.1rem] font-[400] text-white'>
+                  TopUp
+                </span>
+              </div>
+              <div className='ml-3 flex h-auto w-auto flex-col items-center justify-start'>
+                <button
+                  className={`${styles.button} flex h-[80px] w-[80px] items-center justify-center rounded-[25px]`}
+                >
+                  <ArrowDown />
+                </button>
+                <span className='mt-1 cursor-pointer font-poppins text-[1.1rem] font-[400] text-white'>
+                  Withdraw
+                </span>
+              </div>
             </div>
           </div>
         </div>
