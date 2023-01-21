ac696af22b060cfbac2ca8e6cec58fbb82490b05
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2023-01-21 12:19:25 +0530

    updated mobile view

--- src/components/PredictedValue.tsx
+++ src/components/PredictedValue.tsx
@@ -9,14 +9,14 @@ type props = {
 
 const PredictedValue = ({ value, time, index, needSeparator }: props) => {
   return (
-    <div className='relative my-1 flex h-[20px] w-[90%] items-center justify-between pl-3'>
-      <span className='font font-poppins text-[0.9rem] font-[400] text-white'>
+    <div className='relative my-1 flex h-[20px] w-[90%] items-center justify-between pl-0 lg:pl-3'>
+      <span className='font font-poppins text-[0.7rem] font-[400] text-white lg:text-[0.9rem]'>
         {index}. &nbsp;{value}
       </span>
       {needSeparator && (
         <span className='absolute left-1/2 m-auto font-bold text-white'>-</span>
       )}
-      <span className='font font-poppins text-[1rem] font-[400] text-white'>
+      <span className='font font-poppins text-[0.7rem] font-[400] text-white lg:text-[1rem]'>
         {time}
       </span>
     </div>
