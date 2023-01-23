37791b47a306a03d94e9b3111bc3fefb5c9d550e
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2023-01-23 13:21:31 +0530

    updated home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,27 +10,30 @@ import cryptoAnimation from '../../public/lottie/crypto.json';
 export default function HomePage() {
   const router = useRouter();
   return (
-    <div className='relative flex h-screen w-screen flex-col items-center justify-start overflow-hidden bg-backgroundColor pt-[16vh] lg:justify-center lg:pt-0'>
+    <div className='relative flex h-screen w-screen flex-col items-center justify-start overflow-hidden bg-backgroundColor pt-[13vh] lg:justify-center lg:pt-0'>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
       <Header className='pr-[2%]' />
       <Lottie
-        className='absolute bottom-0 -mt-3 h-[50%] lg:right-0 lg:mt-0 lg:h-[80%] lg:w-[60%]'
+        className='absolute bottom-0 -mt-3 -mb-[30%] h-[78%] w-[140%] lg:right-0 lg:mb-0 lg:mt-0 lg:h-[80%] lg:w-[60%]'
         animationData={cryptoAnimation}
         loop={true}
       />
-      <div className='z-[100] flex h-[40%] w-full flex-col items-start justify-center px-10 lg:h-[50%] xxl3100:px-[200px]'>
-        <h1 className='font-poppins text-[3.2rem] font-extrabold leading-[45px] text-button drop-shadow-text lg:text-[3.5rem] lg1300:text-[4.2rem] xl1400:text-[4.5rem] xl1500:text-[6rem] xl1900:text-[7rem] xxl3100:text-[12rem] xxl3100:drop-shadow-textlg'>
-          Predict and Win
+      <div className='z-[100] flex h-[40%] w-full flex-col items-center justify-center px-10 lg:h-[50%] lg:items-start xxl3100:px-[200px]'>
+        <h1 className='flex text-center font-poppins text-[3rem] font-extrabold leading-[45px] text-button drop-shadow-text lg:text-[3.5rem] lg1300:text-[4.2rem] xl1400:text-[4.5rem] xl1500:text-[6rem] xl1900:text-[7rem] xxl3100:text-[12rem] xxl3100:drop-shadow-textlg'>
+          Predict&nbsp; <span className='hidden lg:block'>and</span>
+          <span className='block lg:hidden'>&</span> &nbsp;Win
         </h1>
-        <p className='my-5 ml-1 font-poppins text-[0.9rem] font-[400] tracking-tight text-white lg:text-[1.2rem] lg:leading-7 xl1400:my-8 xl1400:text-[1.5rem] xl1400:leading-8 xxl3100:my-[100px] xxl3100:text-[3rem] xxl3100:leading-[70px]'>
+        <p className='my-5 ml-1 text-center font-poppins text-[0.9rem] font-[300] tracking-tight text-white lg:my-8 lg:text-left lg:text-[1.2rem] lg:leading-7 xl1400:my-8 xl1400:text-[1.5rem] xl1400:leading-8 xxl3100:my-[100px] xxl3100:text-[3rem] xxl3100:leading-[70px]'>
           users can predict the exchange rate of
-          <br /> various crypto currencies at a specific time and win prizes
-          <br /> based on closeness to the actual prize
+          <br className='hidden lg:flex' /> various crypto currencies at a
+          specific time and win prizes
+          <br className='hidden lg:flex' /> based on closeness to the actual
+          prize
         </p>
         <button
           onClick={() => router.push('/contests')}
-          className='h-[35px] w-[50%] max-w-[180px] rounded-[50px] bg-button font-poppins text-[0.9rem] text-white lg:h-[40px] lg:w-[30%] lg:max-w-[150px] lg:text-[1rem] xl1400:h-[50px] xl1400:max-w-[180px] xl1400:text-[1.1rem] xxl3100:h-[100px] xxl3100:max-w-[400px] xxl3100:text-[2.5rem]'
+          className='h-[35px] w-[60%] max-w-[200px] rounded-[50px] bg-button font-poppins text-[0.9rem] text-white lg:h-[40px] lg:w-[30%] lg:max-w-[150px] lg:text-[1rem] xl1400:h-[50px] xl1400:max-w-[180px] xl1400:text-[1.1rem] xxl3100:h-[100px] xxl3100:max-w-[400px] xxl3100:text-[2.5rem]'
         >
           Start predicting
         </button>
